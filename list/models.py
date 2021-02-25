ac5ab8a5dceff45df3987c5bdf205402fa3b57e9
askatanarbaev <askatanarbaevchik@gmail.com>
2021-02-25 18:23:11 +0600

    and one time makemigrate

--- list/models.py
+++ list/models.py
@@ -13,26 +13,29 @@ class Profile(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user
+        return self.first_name
 
 
 class PostImage(models.Model):
     image = models.ImageField(upload_to='postgram/')
     image_description = models.TextField(blank=True)
-    image_likes = models.ManyToManyField('Profile', default=False, blank=True, related_name='likes')
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='create_image')
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='update_image')
+    uploader_of_image = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_by = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name='create_image')
+    updated_by = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name='update_image')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.image_description
 
+    class Meta:
+        ordering = ('created_at',)
+
 
 class Comments(models.Model):
     comment_post = models.CharField(max_length=300)
-    author_comment = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='author_comment')
-    comment_image = models.ForeignKey('PostImage', on_delete=models.CASCADE, related_name='comments')
+    author_comment = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='author_comment')
+    comment_image = models.ForeignKey(PostImage, on_delete=models.CASCADE, related_name='comments')
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
